0b7a582079433f03c17aa2b17389bec64a66e40f
MoMenne <mpmenne@electronicsystemssupport.com>
2013-04-21 11:46:45 -0500

    Initial Commit

---
+++ Nerdfeed/NerdfeedAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NerdfeedAppDelegate.h
+//  Nerdfeed
+//
+//  Created by Mike Menne on 4/21/13.
+//  Copyright (c) 2013 com.bignerdranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by Mike Menne on 4/21/13.
+//  Copyright (c) 2013 com.bignerdranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NerdfeedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NerdfeedAppDelegate class]));
+    }
+}
